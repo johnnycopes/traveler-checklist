b40234bb4c7283e5d5926688635cdf5f9555ac90
John Coppola <jdcoppola@gmail.com>
2017-12-02 12:57:14 -0500

    more webpack modifications and some project architecture restructuring

---
+++ src/index.js
@@ -0,0 +1,5 @@
+require('./scss/main.scss');
+
+var name = 'john';
+console.log(`hello ${name}`);
+document.write(`hello ${name}`);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const webpack = require('webpack');
+const path = require('path');
 
 let config = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
+    path: path.resolve(__dirname, './public'),
     filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // files ending with .js
+        exclude: /node_modules/, // exclude the node modules directory
+        use: {
+          loader: 'babel-loader', // use this loader
+          options: {
+            presets: ['env']
+          }
+        }
+      },
+      {
+        test: /\.scss$/, // files ending with .scss
+        loader: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
   }
 };
 
