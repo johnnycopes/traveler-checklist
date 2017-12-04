6c05f364438f1b509d26bd398be2a4e51e32f948
John Coppola <jdcoppola@gmail.com>
2017-12-04 15:20:29 -0500

    refactors current code into app and ui modules

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,63 @@
-const categoryNames = document.querySelectorAll('.category__name');
-const $categoryLists = document.querySelectorAll('.category__list');
+var UI = setUpUI();
+UI.init();
 
-const documents = document.querySelector('.documents');
-const clothing = document.querySelector('.clothing');
-const toiletries = document.querySelector('.toiletries');
-const technology = document.querySelector('.technology');
+var App = setUpApp(UI);
 
-var itemNames;
-var itemCheckboxes;
+App.setCounter();
+App.generateLists();
 
-const counterChecked = document.querySelector('.counter__checked');
-const counterTotal = document.querySelector('.counter__total');
+// ********************
 
-var categoryLists = {
-  documents,
-  clothing,
-  toiletries,
-  technology
-};
+function setUpUI() {
+  var counterChecked;
+  var counterTotal;
+  var documents;
+  var clothing;
+  var toiletries;
+  var electronics;
+  var misc;
+  var categoryNames;
+  var categoryLists;
+  var categoryListLookup;
 
-for (let categoryName of categoryNames) {
-  categoryName.addEventListener('click', function() {
-    let list = this.nextElementSibling;
+  var publicAPI = {
+    init: initUI,
+    createItemAndAddToCategoryList,
+    updateCounter
+  }
+  
+  return publicAPI;
+
+  // ********************
+
+  function initUI() {
+    counterChecked = document.querySelector('.counter__checked');
+    counterTotal = document.querySelector('.counter__total');
+
+    documents = document.querySelector('.documents');
+    clothing = document.querySelector('.clothing');
+    toiletries = document.querySelector('.toiletries');
+    electronics = document.querySelector('.electronics');
+    misc = document.querySelector('.misc');
+
+    categoryNames = document.querySelectorAll('.category__name');
+    categoryLists = document.querySelectorAll('.category__list');
+
+    categoryListLookup = {
+      documents,
+      clothing,
+      toiletries,
+      electronics,
+      misc
+    };
+
+    for (let categoryName of categoryNames) {
+      categoryName.addEventListener('click', toggleCategoryList);
+    }
+  }
+
+  function toggleCategoryList() {
+    var list = this.nextElementSibling;
     list.classList.toggle('open');
     if (list.style.maxHeight) {
       list.style.maxHeight = null;
@@ -29,147 +65,10 @@ for (let categoryName of categoryNames) {
     else {
       list.style.maxHeight = `${list.scrollHeight}px`;
     }
-  });
-}
-
-var tasklistItems = [
-  {
-    name: 'toothbrush',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'toothpaste',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'floss',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'soap',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'shampoo',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'face cream',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'glasses cleaner',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'razor',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'ID',
-    category: 'documents',
-    completed: false
-  },
-  {
-    name: 'athletic shoes',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'athletic shorts',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'nail clippers',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'comb',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'pomade',
-    category: 'toiletries',
-    completed: false
-  },
-  {
-    name: 'underwear',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'pants',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'shorts',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'shirts',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'sweaters',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'raincoat',
-    category: 'clothing',
-    completed: false
-  },
-  {
-    name: 'laptop',
-    category: 'technology',
-    completed: false
-  },
-  {
-    name: 'phone',
-    category: 'technology',
-    completed: false
-  },
-  {
-    name: 'chargers',
-    category: 'technology',
-    completed: false
-  },
-  {
-    name: 'USB cables',
-    category: 'technology',
-    completed: false
-  },
-  {
-    name: 'Kindle',
-    category: 'technology',
-    completed: false
   }
-];
 
-generateLists();
-setTaskEventListeners();
-updateCounter();
-
-// ********************
-// ********************
-
-function generateLists() {
-  tasklistItems.forEach(item => {
-    var list = categoryLists[item.category];
+  function createItemAndAddToCategoryList(item) {
+    var list = categoryListLookup[item.category];
     var node = document.createElement('div');
     var content = `
       <input class="item__checkbox" type="checkbox" id="${item.name}" />
@@ -177,19 +76,177 @@ function generateLists() {
     `;
     node.classList.add('item');
     node.innerHTML = content;
-    node.addEventListener('click', updateCounter);
+    node.addEventListener('change', notifyCounter);
     list.appendChild(node);
-  });
+  }
+
+  function notifyCounter() {
+    var itemCheckbox = this.children[0];
+    App.updateCounter(itemCheckbox.checked);
+  }
+
+  function updateCounter(counter) {
+    counterChecked.innerText = counter.checked;
+    counterTotal.innerText = counter.total;
+  }
 }
 
-function updateCounter() {
-  itemCheckboxes = document.querySelectorAll('.item__checkbox');
-  var completedItems = 0;
-  itemCheckboxes.forEach(checkbox => {
-    if (checkbox.checked) {
-      completedItems++;
+// ********************
+
+function setUpApp(UI) {
+  var counter = {};
+  var tasklistItems = [
+    {
+      name: 'toothbrush',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'toothpaste',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'floss',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'soap',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'shampoo',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'face cream',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'glasses cleaner',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'razor',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'ID',
+      category: 'documents',
+      completed: false
+    },
+    {
+      name: 'athletic shoes',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'athletic shorts',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'nail clippers',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'comb',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'pomade',
+      category: 'toiletries',
+      completed: false
+    },
+    {
+      name: 'underwear',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'pants',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'shorts',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'shirts',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'sweaters',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'raincoat',
+      category: 'clothing',
+      completed: false
+    },
+    {
+      name: 'laptop',
+      category: 'electronics',
+      completed: false
+    },
+    {
+      name: 'phone',
+      category: 'electronics',
+      completed: false
+    },
+    {
+      name: 'chargers',
+      category: 'electronics',
+      completed: false
+    },
+    {
+      name: 'USB cables',
+      category: 'electronics',
+      completed: false
+    },
+    {
+      name: 'Kindle',
+      category: 'electronics',
+      completed: false
     }
-  });
-  counterChecked.innerText = completedItems;
-  counterTotal.innerText = tasklistItems.length;
+  ];
+
+  var publicAPI = {
+    generateLists,
+    setCounter,
+    updateCounter
+  };
+
+  return publicAPI;
+
+  // ********************
+
+  function generateLists() {
+    tasklistItems.forEach(item => {
+      UI.createItemAndAddToCategoryList(item);
+    });
+  }
+
+  function setCounter() {
+    counter.checked = 0;
+    counter.total = tasklistItems.length;
+    UI.updateCounter(counter);
+  }
+
+  function updateCounter(checkboxIsChecked) {
+    checkboxIsChecked ? counter.checked++ : counter.checked--;
+    UI.updateCounter(counter);
+  }
 }
