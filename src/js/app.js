cc08833f62682c1b59a76c40294cb81a1f773962
John Coppola <jdcoppola@gmail.com>
2017-12-03 21:47:02 -0500

    counter up and running

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
-const categoryNames = document.querySelectorAll('.category-name');
-const $categoryLists = document.querySelectorAll('.category-list');
+const categoryNames = document.querySelectorAll('.category__name');
+const $categoryLists = document.querySelectorAll('.category__list');
 
 const documents = document.querySelector('.documents');
 const clothing = document.querySelector('.clothing');
 const toiletries = document.querySelector('.toiletries');
 const technology = document.querySelector('.technology');
+
+var itemNames;
+var itemCheckboxes;
+
+const counterChecked = document.querySelector('.counter__checked');
+const counterTotal = document.querySelector('.counter__total');
 
 var categoryLists = {
   documents,
@@ -30,131 +36,133 @@ var tasklistItems = [
   {
     name: 'toothbrush',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'toothpaste',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'floss',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'soap',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'shampoo',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'face cream',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'glasses cleaner',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'razor',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'ID',
     category: 'documents',
-    checked: false
+    completed: false
   },
   {
     name: 'athletic shoes',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'athletic shorts',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'nail clippers',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'comb',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'pomade',
     category: 'toiletries',
-    checked: false
+    completed: false
   },
   {
     name: 'underwear',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'pants',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'shorts',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'shirts',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'sweaters',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'raincoat',
     category: 'clothing',
-    checked: false
+    completed: false
   },
   {
     name: 'laptop',
     category: 'technology',
-    checked: false
+    completed: false
   },
   {
     name: 'phone',
     category: 'technology',
-    checked: false
+    completed: false
   },
   {
     name: 'chargers',
     category: 'technology',
-    checked: false
+    completed: false
   },
   {
     name: 'USB cables',
     category: 'technology',
-    checked: false
+    completed: false
   },
   {
     name: 'Kindle',
     category: 'technology',
-    checked: false
+    completed: false
   }
 ];
 
 generateLists();
+setTaskEventListeners();
+updateCounter();
 
 // ********************
 // ********************
@@ -162,19 +170,26 @@ generateLists();
 function generateLists() {
   tasklistItems.forEach(item => {
     var list = categoryLists[item.category];
-    console.log(list);
     var node = document.createElement('div');
     var content = `
-      <input type="checkbox" id="${item.name}" />
+      <input class="item__checkbox" type="checkbox" id="${item.name}" />
       <label class="item__name" for="${item.name}">${item.name}</label>
-      <i class="fa fa-trash-o" aria-hidden="true"></i>
     `;
     node.classList.add('item');
     node.innerHTML = content;
+    node.addEventListener('click', updateCounter);
     list.appendChild(node);
   });
 }
 
-function appendItemToList(list, node) {
-  list.appendChild(node);
+function updateCounter() {
+  itemCheckboxes = document.querySelectorAll('.item__checkbox');
+  var completedItems = 0;
+  itemCheckboxes.forEach(checkbox => {
+    if (checkbox.checked) {
+      completedItems++;
+    }
+  });
+  counterChecked.innerText = completedItems;
+  counterTotal.innerText = tasklistItems.length;
 }
